1cf61560c3c4c19a2c3a60b18629722a716186cf
Branislav Lazic <brano2411@hotmail.com>
2024-09-26 19:26:56 +0200

    Refactor command runner

--- cmd/commands.go
+++ cmd/commands.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"github.com/codeengio/idi/generator"
+	"github.com/rs/zerolog"
+	"io/fs"
 	"slices"
 	"strings"
 
@@ -11,8 +14,18 @@ import (
 
 var allowedArgs = []string{"new"}
 
-func NewRootCmd(run func(cmd *cobra.Command, args []string) error) *cobra.Command {
-	return &cobra.Command{
+type AppRunner struct {
+	logger     zerolog.Logger
+	templateFS fs.FS
+	appGen     *generator.App
+}
+
+func NewAppRunner(appGen *generator.App, logger zerolog.Logger, templateFS fs.FS) *AppRunner {
+	return &AppRunner{appGen: appGen, logger: logger, templateFS: templateFS}
+}
+
+func (a *AppRunner) NewAppCmd() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:   "new",
 		Short: "Creates a new app",
 		Long:  `Creates a new app`,
@@ -28,10 +41,50 @@ func NewRootCmd(run func(cmd *cobra.Command, args []string) error) *cobra.Comman
 			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			err := run(cmd, args)
+			err := a.runNewApp(cmd, args)
 			if err != nil {
 				return
 			}
 		},
 	}
+	cmd.PersistentFlags().StringP("name", "n", "", "app name")
+	cmd.PersistentFlags().StringP("module", "m", "", "Go module name (example.com/module/app)")
+
+	return cmd
+}
+
+func (a *AppRunner) runNewApp(cmd *cobra.Command, _ []string) error {
+	templates := map[string]string{
+		"README.md":              "templates/readme.md.tmpl",
+		"main.go":                "templates/main.go.tmpl",
+		"go.mod":                 "templates/go.mod.tmpl",
+		"doc.go":                 "templates/doc.go.tmpl",
+		"config/config.go":       "templates/config/config.go.tmpl",
+		"db/db.go":               "templates/db/db.go.tmpl",
+		"db/migration.go":        "templates/db/migration.go.tmpl",
+		"db/migrations/init.sql": "templates/db/migrations/init.sql.tmpl",
+		"internal/api/router.go": "templates/internal/api/router.go.tmpl",
+		"docker-compose.yml":     "templates/docker-compose.yml.tmpl",
+		"Makefile":               "templates/makefile.tmpl",
+		".gitignore":             "templates/gitignore.tmpl",
+		".env":                   "templates/env.tmpl",
+		"pkg/hanko/auth.go":      "templates/pkg/hanko/auth.go.tmpl",
+	}
+	name, err := cmd.Flags().GetString("name")
+	if err != nil {
+		return err
+	}
+
+	module, err := cmd.Flags().GetString("module")
+	if err != nil {
+		return err
+	}
+
+	err = a.appGen.GenerateNew(name, module, templates, a.templateFS)
+	if err != nil {
+		a.logger.Error().Err(err).Msg("failed to generate new app")
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/codeengio/idi/cmd"
 	"github.com/codeengio/idi/generator"
 	"github.com/rs/zerolog"
-	"github.com/spf13/cobra"
 )
 
 //go:embed all:templates
@@ -19,52 +18,12 @@ func main() {
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	log := zerolog.New(output).With().Timestamp().Logger()
 
-	rootCmd := cmd.NewRootCmd(runNewApp(log))
-	rootCmd.PersistentFlags().StringP("name", "n", "", "app name")
-	rootCmd.PersistentFlags().StringP("module", "m", "", "Go module name (example.com/module/app)")
+	appGen := generator.NewApp(log, writer.NewFS(log))
+	appRunner := cmd.NewAppRunner(appGen, log, templateFS)
+	rootCmd := appRunner.NewAppCmd()
 
 	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to execute the command")
-	}
-}
-
-func runNewApp(logger zerolog.Logger) func(*cobra.Command, []string) error {
-	templates := map[string]string{
-		"README.md":              "templates/readme.md.tmpl",
-		"main.go":                "templates/main.go.tmpl",
-		"go.mod":                 "templates/go.mod.tmpl",
-		"doc.go":                 "templates/doc.go.tmpl",
-		"config/config.go":       "templates/config/config.go.tmpl",
-		"db/db.go":               "templates/db/db.go.tmpl",
-		"db/migration.go":        "templates/db/migration.go.tmpl",
-		"db/migrations/init.sql": "templates/db/migrations/init.sql.tmpl",
-		"internal/api/router.go": "templates/internal/api/router.go.tmpl",
-		"docker-compose.yml":     "templates/docker-compose.yml.tmpl",
-		"Makefile":               "templates/makefile.tmpl",
-		".gitignore":             "templates/gitignore.tmpl",
-		".env":                   "templates/env.tmpl",
-		"pkg/hanko/auth.go":      "templates/pkg/hanko/auth.go.tmpl",
-	}
-
-	return func(cmd *cobra.Command, args []string) error {
-		name, err := cmd.Flags().GetString("name")
-		if err != nil {
-			return err
-		}
-
-		module, err := cmd.Flags().GetString("module")
-		if err != nil {
-			return err
-		}
-
-		appGen := generator.NewApp(logger, writer.NewFS(logger))
-		err = appGen.GenerateNew(name, module, templates, templateFS)
-		if err != nil {
-			logger.Error().Err(err).Msg("failed to generate new app")
-			return err
-		}
-
-		return nil
 	}
 }
